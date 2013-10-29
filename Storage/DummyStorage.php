f9d2f6595287b558aae6b47f7c8940f1b25c95cb
Buga Dániel <daniel@bugadani.hu>
2013-10-29 21:05:40 +0100

    Add DummyStorage

---
+++ Storage/DummyStorage.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * This file is part of the Miny framework.
+ * (c) Dániel Buga <daniel@bugadani.hu>
+ *
+ * For licensing information see the LICENSE file.
+ */
+
+namespace Modules\OAuth\Storage;
+
+/**
+ * DummyStorage is implemented for cases
+ * when storing OAuth state is not necessary by the module.
+ *
+ * @author Dániel Buga
+ */
+class DummyStorage implements iPersistentStorage {
+
+    /**
+     * @var array
+     */
+    private $data;
+
+    public function __construct() {
+        $this->data = array();
+    }
+
+    public function toArray() {
+        return $this->data;
+    }
+
+    public function &__get($key) {
+        return $this->data[$key];
+    }
+
+    public function __isset($key) {
+        return isset($this->data[$key]);
+    }
+
+    public function __set($key, $value) {
+        $this->data[$key] = $value;
+    }
+
+    public function __unset($key) {
+        unset($this->data[$key]);
+    }
+
+}
