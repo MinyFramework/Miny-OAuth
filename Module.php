fa5fb508a6921a317ee6815ce8379564bbcb6345
Buga Dániel <daniel@bugadani.hu>
2014-01-20 10:56:20 +0100

    Add default configuration.

--- Module.php
+++ Module.php
@@ -14,6 +14,16 @@ use Miny\Application\BaseApplication;
 class Module extends \Miny\Modules\Module
 {
 
+    public function defaultConfiguration()
+    {
+        return array(
+            'oauth' => array(
+                'request_data' => '&request->get',
+                'request_path' => '&request->path'
+            )
+        );
+    }
+
     public function init(BaseApplication $app)
     {
         $app->getFactory()->add('oauth', __NAMESPACE__ . '\OAuthWrapper')
